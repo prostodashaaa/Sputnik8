5e93dd7d96d987ae01f2bc1d04f0a00df8a43cde
Darya <kozyreva2007sv@gmail.com>
2024-11-28 14:32:59 +0300

    Installing Vue & Layot of the header

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div id="app">
+    <header class='header'>
+      <div class='header__logo'><img src="./components/icons/logo.svg" alt="Логотип"></div>
+      <h1 class='header__h1'>Экскурсии по всему миру</h1>
+    </header>
+  </div>
+</template>
+
+<style scoped src="./assets/main.css"></style>
